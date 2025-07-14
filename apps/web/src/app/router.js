dd184cd8209c6fdfe63c481488beab096c0e1af9
gmmagruder <gabrielle.magruder@gmail.com>
2025-07-14 13:34:30 +0100

    refactor: remove notify controller and put placeholder to redirect notify calls to home

--- apps/web/src/app/router.js
+++ apps/web/src/app/router.js
@@ -6,7 +6,6 @@ import { createErrorRoutes } from './views/static/error/index.js';
 import { cacheNoCacheMiddleware } from '@pins/inspector-programming-lib/middleware/cache.js';
 import { buildPostHome, buildViewHome } from './views/home/controller.js';
 import { asyncHandler } from '@pins/inspector-programming-lib/util/async-handler.js';
-// import { buildNotify } from './views/notify/controller.js';
 import { buildViewCase } from './views/case/controller.js';
 import { buildEntraClientMiddleware } from '@pins/inspector-programming-lib/middleware/entra-client.js';
 import { buildViewInspector } from './views/inspector/controller.js';
@@ -49,71 +48,15 @@ export function buildRouter(service) {
 
 	const entraClientMiddleware = buildEntraClientMiddleware(service);
 	const viewHome = buildViewHome(service);
-	// const viewNotify = buildNotify(service);
 	const postHome = buildPostHome(service);
 	const viewCase = buildViewCase(service);
 	const viewInspector = buildViewInspector(service);
 
 	router.get('/', entraClientMiddleware, asyncHandler(viewHome));
-	// router.post('/notify', entraClientMiddleware, asyncHandler(viewNotify));
+	router.post('/notify', (req, res) => res.redirect('/'));
 	router.post('/', asyncHandler(postHome));
 	router.get('/case/:caseId', asyncHandler(viewCase));
 	router.get('/inspector/:inspectorId', entraClientMiddleware, asyncHandler(viewInspector));
 
 	return router;
 }
-
-// import { Router as createRouter } from 'express';
-// import { createRoutesAndGuards as createAuthRoutesAndGuards } from './auth/router.js';
-// import { createMonitoringRoutes } from '@pins/inspector-programming-lib/controllers/monitoring.js';
-// import { buildPostHome, buildViewHome } from './views/home/controller.js';
-
-// /**
-//  * @param {Object} params
-//  * @param {import('pino').BaseLogger} params.logger
-//  * @param {import('./config-types.js').Config} params.config
-//  * @returns {import('express').Router}
-//  */
-// export function buildRouter({ logger, config }) {
-// 	const router = createRouter();
-// 	const monitoringRoutes = createMonitoringRoutes({
-// 		config,
-// 		logger
-// 	});
-// 	const { router: authRoutes, guards: authGuards } = createAuthRoutesAndGuards({
-// 		config,
-// 		logger
-// 	});
-
-// 	router.use('/', monitoringRoutes);
-// 	router.get('/unauthenticated', (req, res) => res.status(401).render('views/errors/401.njk'));
-
-// 	if (!config.auth.disabled) {
-// 		logger.info('registering auth routes');
-// 		router.use('/auth', authRoutes);
-
-// 		// all subsequent routes require auth
-
-// 		// check logged in
-// 		router.use(authGuards.assertIsAuthenticated);
-// 		// check group membership
-// 		router.use(authGuards.assertGroupAccess);
-// 	} else {
-// 		logger.warn('auth disabled; auth routes and guards skipped');
-// 	}
-
-// 	const entraClientMiddleware = buildEntraClientMiddleware({ logger });
-// 	const viewHome = buildViewHome({ logger, config });
-// 	const viewNotify = buildNotify({ logger, config });
-// 	const postHome = buildPostHome({ logger });
-// 	const viewCase = buildViewCase({ logger, config });
-// 	const viewInspector = buildViewInspector({ logger, config });
-
-// 	router.get('/', entraClientMiddleware, asyncHandler(viewHome));
-// 	router.post('/notify', entraClientMiddleware, asyncHandler(viewNotify));
-// 	router.post('/', asyncHandler(postHome));
-// 	router.get('/case/:caseId', asyncHandler(viewCase));
-// 	router.get('/inspector/:inspectorId', entraClientMiddleware, asyncHandler(viewInspector));
-
-// 	return router;
-// }
