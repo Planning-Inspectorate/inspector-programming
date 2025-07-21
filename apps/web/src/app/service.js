1a7b16642af475c18916ac8b897345c0eb37b508
saalimaaliyammu <saalima.aliyammu.1c@planninginspectorate.gov.uk>
2025-07-21 18:31:24 +0100

    chore: case fetching CBOS

--- apps/web/src/app/service.js
+++ apps/web/src/app/service.js
@@ -2,7 +2,8 @@ import { BaseService } from '@pins/inspector-programming-lib/app/base-service.js
 import { buildInitEntraClient } from '@pins/inspector-programming-lib/graph/cached-entra-client.js';
 import { MapCache } from '@pins/inspector-programming-lib/util/map-cache.js';
 import { ApiService } from './api/api-service.js';
-
+import { CbosApiClient } from '@pins/inspector-programming-lib/data/cbos/cbos-api-client.js';
+import { getAccountId } from '../util/account.js';
 /**
  * This class encapsulates all the services and clients for the application
  */
@@ -21,7 +22,6 @@ export class WebService extends BaseService {
 		this.#config = config;
 		const entraGroupCache = new MapCache(config.entra.cacheTtl);
 		this.entraClient = buildInitEntraClient(!config.auth.disabled, entraGroupCache);
-
 		this.apiService = new ApiService(this);
 	}
 
@@ -30,6 +30,18 @@ export class WebService extends BaseService {
 	 */
 	get authConfig() {
 		return this.#config.auth;
+	}
+
+	/**
+	 * Returns a cbosApiClient instance for the current user session.
+	 * @param {object} session - The current request session
+	 */
+	getCbosApiClientForSession(session) {
+		const userId = getAccountId(session);
+		return new CbosApiClient({
+			...this.#config.cbos,
+			apiHeader: userId
+		});
 	}
 
 	get entraConfig() {
