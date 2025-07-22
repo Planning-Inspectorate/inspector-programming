cc3f1c3d74a690856a9a353698ff66817e1a47a7
gmmagruder <gabrielle.magruder@gmail.com>
2025-07-22 12:23:52 +0100

    test: add unit tests for inspector data and account utility functions

--- apps/web/src/app/inspector/inspector.test.js
+++ apps/web/src/app/inspector/inspector.test.js
@@ -1,137 +1,277 @@
-import { describe, it } from 'node:test';
+import { describe, it, mock } from 'node:test';
 import { strict as assert } from 'node:assert';
-import { sortInspectorList } from './inspector.js';
+import { getInspectorById, getInspectorList, getSortedInspectorList } from './inspector.js';
+
+const groupId = 'groupId';
+const mockSession = {};
+const mockLogger = {
+	warn: mock.fn()
+};
+
+const mockEntraClient = {
+	listAllGroupMembers: mock.fn()
+};
+
+const mockInitEntraClient = mock.fn();
+mockInitEntraClient.mock.mockImplementation(() => mockEntraClient);
 
 describe('inspectors', () => {
-	it('should sort inspectors list by last name', () => {
-		let inspector1 = {
-			id: '0',
-			firstName: 'John',
-			lastName: 'Baker',
-			emailAddress: 'test@email.co,m',
-			address: {
-				addressLine1: 'address line 1',
-				addressLine2: 'address line 2',
-				townCity: 'town',
-				county: 'county',
-				postcode: 'postcode'
+	it('should fetch unsorted inspector list', async () => {
+		const groupMemberList = [
+			{
+				id: '0',
+				givenName: 'John',
+				surname: 'Baker',
+				mail: 'test@email.com'
 			},
-			grade: '0',
-			fte: 1,
-			inspectorManager: false,
-			chartingOfficerId: '0',
-			specialisms: [],
-			preclusions: []
-		};
-
-		let inspector2 = {
-			id: '0',
-			firstName: 'John',
-			lastName: 'Adams',
-			emailAddress: 'test@email.co,m',
-			address: {
-				addressLine1: 'address line 1',
-				addressLine2: 'address line 2',
-				townCity: 'town',
-				county: 'county',
-				postcode: 'postcode'
+			{
+				id: '1',
+				givenName: 'John',
+				surname: 'Adams',
+				mail: 'test@email.com'
 			},
-			grade: '0',
-			fte: 1,
-			inspectorManager: false,
-			chartingOfficerId: '0',
-			specialisms: [],
-			preclusions: []
-		};
+			{
+				id: '2',
+				givenName: 'John',
+				surname: 'Allen',
+				mail: 'test@email.com'
+			}
+		];
 
-		let inspector3 = {
-			id: '0',
-			firstName: 'John',
-			lastName: 'Allen',
-			emailAddress: 'test@email.co,m',
-			address: {
-				addressLine1: 'address line 1',
-				addressLine2: 'address line 2',
-				townCity: 'town',
-				county: 'county',
-				postcode: 'postcode'
+		const expectedInspectorList = [
+			{
+				id: '0',
+				firstName: 'John',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
 			},
-			grade: '0',
-			fte: 1,
-			inspectorManager: false,
-			chartingOfficerId: '0',
-			specialisms: [],
-			preclusions: []
-		};
+			{
+				id: '1',
+				firstName: 'John',
+				lastName: 'Adams',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '2',
+				firstName: 'John',
+				lastName: 'Allen',
+				emailAddress: 'test@email.com'
+			}
+		];
 
-		let inspectorList = [inspector1, inspector2, inspector3];
-		let sortedList = sortInspectorList(inspectorList);
-		assert.deepStrictEqual(sortedList, [inspector2, inspector3, inspector1]);
+		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
+		const inspectorList = await getInspectorList(mockInitEntraClient, mockSession, mockLogger, groupId);
+		assert.deepStrictEqual(inspectorList, expectedInspectorList);
 	});
 
-	it('should sort inspectors list by first name', () => {
-		let inspector1 = {
-			id: '0',
-			firstName: 'John',
-			lastName: 'Baker',
-			emailAddress: 'test@email.co,m',
-			address: {
-				addressLine1: 'address line 1',
-				addressLine2: 'address line 2',
-				townCity: 'town',
-				county: 'county',
-				postcode: 'postcode'
+	it('should return empty inspector list if client is undefined', async () => {
+		mockInitEntraClient.mock.mockImplementationOnce(() => undefined);
+		const inspectorList = await getInspectorList(mockInitEntraClient, mockSession, mockLogger, groupId);
+		assert.deepStrictEqual(inspectorList, []);
+	});
+
+	it('should fetch inspector list sorted by last name', async () => {
+		const groupMemberList = [
+			{
+				id: '0',
+				givenName: 'John',
+				surname: 'Baker',
+				mail: 'test@email.com'
 			},
-			grade: '0',
-			fte: 1,
-			inspectorManager: false,
-			chartingOfficerId: '0',
-			specialisms: [],
-			preclusions: []
+			{
+				id: '1',
+				givenName: 'John',
+				surname: 'Adams',
+				mail: 'test@email.com'
+			},
+			{
+				id: '2',
+				givenName: 'John',
+				surname: 'Allen',
+				mail: 'test@email.com'
+			}
+		];
+
+		const expectedInspectorList = [
+			{
+				id: '1',
+				firstName: 'John',
+				lastName: 'Adams',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '2',
+				firstName: 'John',
+				lastName: 'Allen',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '0',
+				firstName: 'John',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			}
+		];
+
+		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
+		const inspectorList = await getSortedInspectorList(mockInitEntraClient, mockSession, mockLogger, groupId);
+		assert.deepStrictEqual(inspectorList, expectedInspectorList);
+	});
+
+	it('should fetch inspector list sorted by first name', async () => {
+		const groupMemberList = [
+			{
+				id: '0',
+				givenName: 'John',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			},
+			{
+				id: '1',
+				givenName: 'Alice',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			},
+			{
+				id: '2',
+				givenName: 'Jake',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			}
+		];
+
+		const expectedInspectorList = [
+			{
+				id: '1',
+				firstName: 'Alice',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '2',
+				firstName: 'Jake',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '0',
+				firstName: 'John',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			}
+		];
+
+		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
+		const inspectorList = await getSortedInspectorList(mockInitEntraClient, mockSession, mockLogger, groupId);
+		assert.deepStrictEqual(inspectorList, expectedInspectorList);
+	});
+
+	it('should fetch inspector list sorted by last name then first name', async () => {
+		const groupMemberList = [
+			{
+				id: '0',
+				givenName: 'John',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			},
+			{
+				id: '1',
+				givenName: 'Liam',
+				surname: 'Adams',
+				mail: 'test@email.com'
+			},
+			{
+				id: '2',
+				givenName: 'Jake',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			}
+		];
+
+		const expectedInspectorList = [
+			{
+				id: '1',
+				firstName: 'Liam',
+				lastName: 'Adams',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '2',
+				firstName: 'Jake',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '0',
+				firstName: 'John',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			}
+		];
+
+		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
+		const inspectorList = await getSortedInspectorList(mockInitEntraClient, mockSession, mockLogger, groupId);
+		assert.deepStrictEqual(inspectorList, expectedInspectorList);
+	});
+
+	it('should get inspector by id', async () => {
+		const groupMemberList = [
+			{
+				id: '0',
+				givenName: 'John',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			},
+			{
+				id: '1',
+				givenName: 'Liam',
+				surname: 'Adams',
+				mail: 'test@email.com'
+			},
+			{
+				id: '2',
+				givenName: 'Jake',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			}
+		];
+
+		const expectedInspector = {
+			id: '1',
+			firstName: 'Liam',
+			lastName: 'Adams',
+			emailAddress: 'test@email.com'
 		};
 
-		let inspector2 = {
-			id: '0',
-			firstName: 'Alice',
-			lastName: 'Baker',
-			emailAddress: 'test@email.co,m',
-			address: {
-				addressLine1: 'address line 1',
-				addressLine2: 'address line 2',
-				townCity: 'town',
-				county: 'county',
-				postcode: 'postcode'
-			},
-			grade: '0',
-			fte: 1,
-			inspectorManager: false,
-			chartingOfficerId: '0',
-			specialisms: [],
-			preclusions: []
-		};
+		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
+		const inspector = await getInspectorById(mockInitEntraClient, mockSession, mockLogger, groupId, '1');
+		assert.deepStrictEqual(inspector, expectedInspector);
+	});
 
-		let inspector3 = {
-			id: '0',
-			firstName: 'Jake',
-			lastName: 'Baker',
-			emailAddress: 'test@email.co,m',
-			address: {
-				addressLine1: 'address line 1',
-				addressLine2: 'address line 2',
-				townCity: 'town',
-				county: 'county',
-				postcode: 'postcode'
+	it('should return undefined when inpector id is not in the inspector list', async () => {
+		const groupMemberList = [
+			{
+				id: '0',
+				givenName: 'John',
+				surname: 'Baker',
+				mail: 'test@email.com'
 			},
-			grade: '0',
-			fte: 1,
-			inspectorManager: false,
-			chartingOfficerId: '0',
-			specialisms: [],
-			preclusions: []
-		};
+			{
+				id: '1',
+				givenName: 'Liam',
+				surname: 'Adams',
+				mail: 'test@email.com'
+			},
+			{
+				id: '2',
+				givenName: 'Jake',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			}
+		];
 
-		let inspectorList = [inspector1, inspector2, inspector3];
-		let sortedList = sortInspectorList(inspectorList);
-		assert.deepStrictEqual(sortedList, [inspector2, inspector3, inspector1]);
+		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
+		const inspector = await getInspectorById(mockInitEntraClient, mockSession, mockLogger, groupId, '5');
+		assert.strictEqual(inspector, undefined);
 	});
 });
