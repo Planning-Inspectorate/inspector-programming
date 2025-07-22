20adb75d2ea9701ec270775fec8bef0d03c9c50c
gmmagruder <gabrielle.magruder@gmail.com>
2025-07-22 15:23:32 +0100

    refactor: remove duplicate inspector code and put it into common function

--- apps/web/src/app/inspector/inspector.test.js
+++ apps/web/src/app/inspector/inspector.test.js
@@ -1,6 +1,6 @@
 import { describe, it, mock } from 'node:test';
 import { strict as assert } from 'node:assert';
-import { getInspectorById, getInspectorList, getSortedInspectorList } from './inspector.js';
+import { getInspectorById, fetchInspectorList, getSortedInspectorList, getInspectorList } from './inspector.js';
 
 const groupId = 'groupId';
 const mockSession = {};
@@ -14,6 +14,16 @@ const mockEntraClient = {
 
 const mockInitEntraClient = mock.fn();
 mockInitEntraClient.mock.mockImplementation(() => mockEntraClient);
+
+const mockService = {
+	entraClient: mockInitEntraClient,
+	logger: mockLogger,
+	entraGroupIds: {
+		teamLeads: '0',
+		nationalTeam: '1',
+		inspectors: '2'
+	}
+};
 
 describe('inspectors', () => {
 	it('should fetch unsorted inspector list', async () => {
@@ -60,13 +70,13 @@ describe('inspectors', () => {
 		];
 
 		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
-		const inspectorList = await getInspectorList(mockInitEntraClient, mockSession, mockLogger, groupId);
+		const inspectorList = await fetchInspectorList(mockInitEntraClient, mockSession, mockLogger, groupId);
 		assert.deepStrictEqual(inspectorList, expectedInspectorList);
 	});
 
 	it('should return empty inspector list if client is undefined', async () => {
 		mockInitEntraClient.mock.mockImplementationOnce(() => undefined);
-		const inspectorList = await getInspectorList(mockInitEntraClient, mockSession, mockLogger, groupId);
+		const inspectorList = await fetchInspectorList(mockInitEntraClient, mockSession, mockLogger, groupId);
 		assert.deepStrictEqual(inspectorList, []);
 	});
 
@@ -273,5 +283,162 @@ describe('inspectors', () => {
 		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
 		const inspector = await getInspectorById(mockInitEntraClient, mockSession, mockLogger, groupId, '5');
 		assert.strictEqual(inspector, undefined);
+	});
+
+	it('should return full sorted inspector list when user is team lead', async () => {
+		const groupMemberList = [
+			{
+				id: '0',
+				givenName: 'John',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			},
+			{
+				id: '1',
+				givenName: 'Liam',
+				surname: 'Adams',
+				mail: 'test@email.com'
+			},
+			{
+				id: '2',
+				givenName: 'Jake',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			}
+		];
+
+		const expectedInspectorList = [
+			{
+				id: '1',
+				firstName: 'Liam',
+				lastName: 'Adams',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '2',
+				firstName: 'Jake',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '0',
+				firstName: 'John',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			}
+		];
+
+		const mockSessionWithAccount = {
+			account: {
+				idTokenClaims: {
+					groups: ['0']
+				}
+			}
+		};
+
+		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
+		const inspectorList = await getInspectorList(mockService, mockSessionWithAccount);
+		assert.deepStrictEqual(inspectorList, expectedInspectorList);
+	});
+
+	it('should return full sorted inspector list when user is national team', async () => {
+		const groupMemberList = [
+			{
+				id: '0',
+				givenName: 'John',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			},
+			{
+				id: '1',
+				givenName: 'Liam',
+				surname: 'Adams',
+				mail: 'test@email.com'
+			},
+			{
+				id: '2',
+				givenName: 'Jake',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			}
+		];
+
+		const expectedInspectorList = [
+			{
+				id: '1',
+				firstName: 'Liam',
+				lastName: 'Adams',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '2',
+				firstName: 'Jake',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			},
+			{
+				id: '0',
+				firstName: 'John',
+				lastName: 'Baker',
+				emailAddress: 'test@email.com'
+			}
+		];
+
+		const mockSessionWithAccount = {
+			account: {
+				idTokenClaims: {
+					groups: ['1']
+				}
+			}
+		};
+
+		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
+		const inspectorList = await getInspectorList(mockService, mockSessionWithAccount);
+		assert.deepStrictEqual(inspectorList, expectedInspectorList);
+	});
+
+	it('should return inspector list containing only the current user when user is inspector', async () => {
+		const groupMemberList = [
+			{
+				id: '0',
+				givenName: 'John',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			},
+			{
+				id: '1',
+				givenName: 'Liam',
+				surname: 'Adams',
+				mail: 'test@email.com'
+			},
+			{
+				id: '2',
+				givenName: 'Jake',
+				surname: 'Baker',
+				mail: 'test@email.com'
+			}
+		];
+
+		const expectedInspectorList = [
+			{
+				id: '1',
+				firstName: 'Liam',
+				lastName: 'Adams',
+				emailAddress: 'test@email.com'
+			}
+		];
+
+		const mockSessionWithAccount = {
+			account: {
+				idTokenClaims: {
+					groups: ['2']
+				},
+				localAccountId: '1'
+			}
+		};
+
+		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
+		const inspectorList = await getInspectorList(mockService, mockSessionWithAccount);
+		assert.deepStrictEqual(inspectorList, expectedInspectorList);
 	});
 });
