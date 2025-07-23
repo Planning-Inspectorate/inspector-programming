1d362cf943461b76db60e7877a54762a82d91afc
james.barnes <james.barnes@solirius.com>
2025-07-23 13:58:09 +0100

    refactor: implement EntraClient package into api service and implement for /users

--- apps/web/src/app/api/api-service.js
+++ apps/web/src/app/api/api-service.js
@@ -1,6 +1,9 @@
 import { TokenCredentialAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials/index.js';
 import { DefaultAzureCredential } from '@azure/identity';
 import { Client } from '@microsoft/microsoft-graph-client';
+import { MapCache } from '@pins/inspector-programming-lib/util/map-cache.js';
+import { EntraClient } from '@pins/inspector-programming-lib/graph/entra.js';
+import { CachedEntraClient } from '@pins/inspector-programming-lib/graph/cached-entra-client.js';
 
 export class ApiService {
 	/**
@@ -8,10 +11,16 @@ export class ApiService {
 	 */
 	constructor(service) {
 		this.service = service;
-		this.graphClient = Client.initWithMiddleware({
+		const client = Client.initWithMiddleware({
 			authProvider: new TokenCredentialAuthenticationProvider(new DefaultAzureCredential(), {
 				scopes: ['https://graph.microsoft.com/.default']
 			})
 		});
+
+		//make a new cache
+		const apiCache = new MapCache(service.entraConfig.cacheTtl);
+
+		const entraClient = new EntraClient(client);
+		this.entraClient = new CachedEntraClient(entraClient, apiCache);
 	}
 }
