aea3978d137166e7d5cc73f1a029c3b312f0b476
james.barnes <james.barnes@solirius.com>
2025-08-05 13:06:43 +0100

    feat: implement validation and filtering for min/max age filters

--- apps/web/src/app/views/home/controller.js
+++ apps/web/src/app/views/home/controller.js
@@ -3,6 +3,21 @@ import qs from 'qs';
 import { parse as parseUrl } from 'url';
 
 /**
+ * @typedef {Object} Case
+ * @property {string} caseId - The unique identifier for the case.
+ * @property {string} caseType - The type of the case (e.g., 'W').
+ * @property {string} caseProcedure - The procedure for the case (e.g., 'Written').
+ * @property {string} allocationBand - The allocation band for the case.
+ * @property {string} caseLevel - The level of the case.
+ * @property {string} siteAddressPostcode - The postcode of the site address.
+ * @property {string} lpaName - The name of the Local Planning Authority (LPA).
+ * @property {string} lpaRegion - The region of the Local Planning Authority (LPA).
+ * @property {string} caseStatus - The status of the case
+ * @property {number} caseAge - The age of the case in weeks.
+ * @property {number} linkedCases - The number of linked cases.
+ * @property {Date} finalCommentsDate - The date of the final comments.
+ */
+/**
  * @param {import('#service').WebService} service
  * @returns {import('express').Handler}
  */
@@ -10,24 +25,23 @@ export function buildViewHome(service) {
 	return async (req, res) => {
 		const inspectors = await getInspectorList(service, req.session);
 		const selectedInspector = inspectors.find((i) => req.query.inspectorId === i.id);
-		const errors = {};
 
 		// Convert the raw query string into a nested object
 		const query = qs.parse(parseUrl(req.url).query || '');
 		const { filters } = query;
 
-		if (filters) {
-			if (filters.minimumAge && (isNaN(filters.minimumAge) || filters.minimumAge < 0 || filters.minimumAge > 500)) {
-				errors.minimumAge = 'Please enter a number between 0 and 500';
-			}
-		}
-
 		const page = req.query.page ? parseInt(req.query.page) : 1;
 		const limit = req.query.limit ? parseInt(req.query.limit, 10) : 10;
 		const cases = await service.getCbosApiClientForSession(req.session).getCases();
-		const sortedCases = cases.sort((a, b) => b.caseAge - a.caseAge);
+
+		const errors = validateFilters(filters);
+		const filteredCases = filterCases(cases, filters);
+
+		const sortedCases = filteredCases.sort((a, b) => b.caseAge - a.caseAge);
+
 		const start = (page - 1) * limit;
 		const paginatedCases = sortedCases.slice(start, start + limit);
+
 		const formData = {
 			filters,
 			limit,
@@ -55,6 +69,61 @@ export function buildViewHome(service) {
 			errors
 		});
 	};
+}
+
+/**
+ * @typedef {Object} Filters
+ * //any string value can be used as an age filter, we just validate it later
+ * @property {string} [minimumAge]
+ * @property {string} [maximumAge]
+ */
+
+/**
+ * @typedef {Partial<Object<keyof Filters, { text: string }>>} ValidationErrors
+ */
+
+/**
+ *
+ * @param {Filters} filters
+ * @returns {ValidationErrors}
+ */
+function validateFilters(filters) {
+	/** @type {ValidationErrors} */
+	const errors = {};
+	if (!filters) return errors;
+
+	/** @type {(keyof Filters)[]} */
+	const keysToValidate = ['minimumAge', 'maximumAge'];
+	for (const key of keysToValidate) {
+		const value = filters[key];
+		if (value && (isNaN(+value) || +value <= 0 || +value > 500)) {
+			errors[key] = { text: 'Please enter a number between 0 and 500' };
+			filters[key] = '';
+		}
+	}
+	if (filters.maximumAge && filters.minimumAge && !isNaN(+filters.minimumAge) && !isNaN(+filters.maximumAge)) {
+		if (+filters.minimumAge > +filters.maximumAge) {
+			errors.minimumAge = { text: 'The minimum value must be less than or equal to the maximum value.' };
+			filters.minimumAge = '';
+			filters.maximumAge = '';
+		}
+	}
+	return errors;
+}
+
+/**
+ *
+ * @param {Case[]} cases
+ * @param {Filters} filters
+ * @returns
+ */
+function filterCases(cases, filters) {
+	if (!filters) return cases;
+	return cases.filter((c) => {
+		//always apply case age filters, using defaults if no filter provided
+		if (!(c.caseAge > (+filters.minimumAge || 0) && c.caseAge < (+filters.maximumAge || 500))) return false;
+		return true;
+	});
 }
 
 export function caseViewModel(c) {
