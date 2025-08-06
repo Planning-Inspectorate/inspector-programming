e8599b53f62177a97f00a4e519500b11f6474bf5
james.barnes <james.barnes@solirius.com>
2025-08-06 15:53:09 +0100

    feat: add casesclient to webservice and record return type

--- packages/lib/data/types.d.ts
+++ packages/lib/data/types.d.ts
@@ -7,3 +7,24 @@ export interface FetchCasesResponse {
 	cases: AppealCase[];
 	total: number;
 }
+
+export interface dbCase {
+	caseReference: string;
+	caseStatus: string | null;
+	caseType: string | null;
+	caseProcedure: string | null;
+	allocationLevel: string | null;
+	allocationBand: number | null;
+	siteAddressLine1: string | null;
+	siteAddressLine2?: string | null;
+	siteAddressTown: string | null;
+	siteAddressCounty: string | null;
+	siteAddressPostcode: string | null;
+	lpaCode: string | null;
+	lpaName: string | null;
+	lpaRegion: string | null;
+	caseValidDate: Date | null;
+	finalCommentsDueDate: Date | null;
+	linkedCaseStatus: string | null;
+	leadCaseReference: string | null;
+}
