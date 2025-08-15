20c45c9656505d48e9e21d9c4a0830c2f0205cb8
gmmagruder <gabrielle.magruder@gmail.com>
2025-08-15 13:52:34 +0100

    feat: change errors so that they only appear when calendar tab is selected

--- apps/web/src/app/views/home/controller.js
+++ apps/web/src/app/views/home/controller.js
@@ -82,18 +82,22 @@ export function buildViewHome(service) {
 				service.logger.error(error, 'Failed to fetch calendar events');
 				calendarError =
 					"Can't view this calendar. Please contact the inspector to ensure their calendar is shared with you.";
-				errorSummary.push({
-					text: calendarError,
-					href: '#calendarError'
-				});
+				if (req.query.currentTab == 'calendar') {
+					errorSummary.push({
+						text: calendarError,
+						href: '#calendarError'
+					});
+				}
 			}
 		} else {
 			calendarError = 'No Inspector Selected. Please select an Inspector from the drop down to see this information.';
-			inspectorError = calendarError;
-			errorSummary.push({
-				text: calendarError,
-				href: '#inspectors'
-			});
+			if (req.query.currentTab == 'calendar') {
+				inspectorError = calendarError;
+				errorSummary.push({
+					text: calendarError,
+					href: '#inspectors'
+				});
+			}
 		}
 
 		const currentStartDate = req.query.calendarStartDate
@@ -128,6 +132,7 @@ export function buildViewHome(service) {
 			specialismTypes,
 			caseTypes,
 			inspectorError,
+			calendarError,
 			errorSummary
 		});
 	};
