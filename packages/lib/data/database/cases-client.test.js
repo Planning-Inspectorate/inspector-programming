ad1aa00d89e2d6bebe7e7464ebac4d1c102ef94e
james.barnes <james.barnes@solirius.com>
2025-08-18 15:45:00 +0100

    feat: reset page to 1 when changing sort criteria

--- packages/lib/data/database/cases-client.test.js
+++ packages/lib/data/database/cases-client.test.js
@@ -267,4 +267,144 @@ describe('CasesClient', () => {
 			assert.deepStrictEqual(overflowPageTwo.total, 5);
 		});
 	});
+	describe('sortCasesByAge', () => {
+		const mockCases = [
+			{
+				caseId: 'ref1',
+				caseType: 'W',
+				caseProcedure: 'inquiry',
+				allocationBand: 1,
+				caseLevel: 'H',
+				siteAddressPostcode: 'SY10 7FA',
+				siteAddressLatitude: 52.89835,
+				siteAddressLongitude: -3.064346,
+				lpaName: 'Definitely a Local Planning Authority',
+				lpaRegion: '',
+				caseStatus: 'lpa_questionnaire',
+				caseAge: 5,
+				linkedCases: 0,
+				caseReceivedDate: new Date('2024-10-10T10:26:11.963Z'),
+				finalCommentsDate: new Date('2024-10-10T10:26:11.963Z'),
+				specialisms: undefined,
+				specialismList: 'None'
+			},
+			{
+				caseId: 'ref2',
+				caseType: 'W',
+				caseProcedure: 'inquiry',
+				allocationBand: 1,
+				caseLevel: 'H',
+				siteAddressPostcode: 'SY10 7FA',
+				siteAddressLatitude: 52.89835,
+				siteAddressLongitude: -3.064346,
+				lpaName: 'Certainly a Local Planning Authority',
+				lpaRegion: '',
+				caseStatus: 'lpa_questionnaire',
+				caseAge: 10,
+				linkedCases: 0,
+				caseReceivedDate: new Date('2024-10-08T10:26:11.963Z'),
+				finalCommentsDate: new Date('2024-10-10T10:26:11.963Z'),
+				specialisms: undefined,
+				specialismList: 'None'
+			},
+			{
+				caseId: 'ref3',
+				caseType: 'W',
+				caseProcedure: 'inquiry',
+				allocationBand: 1,
+				caseLevel: 'H',
+				siteAddressPostcode: 'SY10 7FA',
+				siteAddressLatitude: 52.89835,
+				siteAddressLongitude: -3.064346,
+				lpaName: 'A Local Planning Authority',
+				lpaRegion: '',
+				caseStatus: 'lpa_questionnaire',
+				caseAge: 15,
+				linkedCases: 0,
+				caseReceivedDate: new Date('2024-10-09T10:26:11.963Z'),
+				finalCommentsDate: new Date('2024-10-10T10:26:11.963Z'),
+				specialisms: undefined,
+				specialismList: 'None'
+			},
+			{
+				caseId: 'ref4',
+				caseType: 'W',
+				caseProcedure: 'inquiry',
+				allocationBand: 1,
+				caseLevel: 'H',
+				siteAddressPostcode: 'SY10 7FA',
+				siteAddressLatitude: 52.89835,
+				siteAddressLongitude: -3.064346,
+				lpaName: 'Brilliant Local Planning Authority',
+				lpaRegion: '',
+				caseStatus: 'lpa_questionnaire',
+				caseAge: 20,
+				linkedCases: 0,
+				caseReceivedDate: new Date('2024-10-11T10:26:11.963Z'),
+				finalCommentsDate: new Date('2024-10-10T10:26:11.963Z'),
+				specialisms: undefined,
+				specialismList: 'None'
+			},
+			{
+				caseId: 'ref5',
+				caseType: 'W',
+				caseProcedure: 'inquiry',
+				allocationBand: 1,
+				caseLevel: 'H',
+				siteAddressPostcode: 'SY10 7FA',
+				siteAddressLatitude: 52.89835,
+				siteAddressLongitude: -3.064346,
+				lpaName: 'Example Local Planning Authority',
+				lpaRegion: '',
+				caseStatus: 'lpa_questionnaire',
+				caseAge: 3,
+				linkedCases: 0,
+				caseReceivedDate: new Date('2024-10-15T10:26:11.963Z'),
+				finalCommentsDate: new Date('2024-10-14T10:26:11.963Z'),
+				specialisms: undefined,
+				specialismList: 'None'
+			}
+		];
+		it('should fetch three cases sorted by age', async () => {
+			const mockDbClient = {};
+
+			const casesClient = new CasesClient(mockDbClient);
+			const cases = await casesClient.sortCasesByAge(mockCases);
+			assert.deepStrictEqual(
+				cases.map((c) => c.caseId),
+				['ref4', 'ref3', 'ref2', 'ref1', 'ref5']
+			);
+		});
+		it('cases having the same age should sort by caseReceivedDate instead', async () => {
+			const mockDbClient = {};
+
+			mockCases.forEach((c) => {
+				c.caseAge = 5;
+			});
+
+			const casesClient = new CasesClient(mockDbClient);
+			const cases = await casesClient.sortCasesByAge(mockCases);
+
+			assert.deepStrictEqual(
+				cases.map((c) => c.caseId),
+				['ref2', 'ref3', 'ref1', 'ref4', 'ref5']
+			);
+		});
+		it('cases having the same age and caseReceivedDate should sort by LPA name in alphabetical order', async () => {
+			const mockDbClient = {};
+
+			mockCases.forEach((c) => {
+				c.caseAge = 5;
+				c.caseReceivedDate = new Date('2024-10-09T10:26:11.963Z');
+			});
+
+			const casesClient = new CasesClient(mockDbClient);
+			const cases = await casesClient.sortCasesByAge(mockCases);
+
+			assert.deepStrictEqual(
+				cases.map((c) => c.caseId),
+				['ref3', 'ref4', 'ref2', 'ref1', 'ref5']
+			);
+		});
+	});
 });
