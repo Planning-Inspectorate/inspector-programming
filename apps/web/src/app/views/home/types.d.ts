4305e1b9a6c37847f0b3b4731ccbba510e59294e
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-08-20 09:21:57 +0100

    docs: home view model

---
+++ apps/web/src/app/views/home/types.d.ts
@@ -0,0 +1,70 @@
+import { CalendarEntry } from '../../calendar/types';
+import { CaseViewModel } from '@pins/inspector-programming-lib/data/types';
+import { Inspector } from '../../inspector/types';
+import { ErrorSummary, PageViewModel, Pagination, RadioOption, TextValue } from '#util/types';
+
+export interface HomeViewModel extends PageViewModel {
+	filters: Filters;
+	errorSummary?: ErrorSummary[];
+
+	appeals?: AppealsViewModel;
+	calendar?: CalendarViewModel;
+	inspectors?: InspectorsViewModel;
+	map?: MapViewModel;
+}
+
+export interface AppealsViewModel {
+	cases: CaseViewModel[];
+}
+
+export interface CalendarViewModel {
+	currentStartDate: Date;
+	dates: string[];
+	grid: CalendarEntry[][];
+	times: string[];
+	weekTitle: string;
+
+	error?: string;
+}
+
+export interface Filters {
+	allocationLevels: RadioOption[];
+	caseTypes: RadioOption[];
+	specialisms: RadioOption[];
+
+	pagination: Pagination;
+	query: FilterQuery;
+
+	// filter-specific errors, where keys are the filter input name
+	errors?: {
+		minimumAge?: TextValue;
+		maximumAge?: TextValue;
+	};
+}
+
+export interface FilterQuery {
+	// case filters
+	allocationLevel?: string[];
+	caseProcedure?: string[];
+	caseSpecialisms?: string[];
+	caseType?: string[];
+	lpaRegion?: string[];
+	minimumAge?: string;
+	maximumAge?: string;
+
+	limit?: number;
+	page?: number;
+	sort?: string | 'age' | 'distance' | 'hybrid';
+	inspectorId?: string;
+}
+
+export interface InspectorsViewModel {
+	list: Inspector[];
+	selected?: Inspector;
+
+	error?: string;
+}
+
+export interface MapViewModel {
+	apiKey: string;
+}
