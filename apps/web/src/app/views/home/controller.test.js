e2a5dd6d69184b66dbeb540c6f3cf7ee882320b0
gmmagruder <gabrielle.magruder@gmail.com>
2025-08-20 18:23:25 +0100

    test: add tests for patch cases api call

--- apps/web/src/app/views/home/controller.test.js
+++ apps/web/src/app/views/home/controller.test.js
@@ -1,5 +1,5 @@
-import { describe, mock, test } from 'node:test';
-import { buildPostHome, buildViewHome } from './controller.js';
+import { beforeEach, describe, mock, test } from 'node:test';
+import { buildPostCases, buildPostHome, buildViewHome } from './controller.js';
 import assert from 'assert';
 import { mockLogger } from '@pins/inspector-programming-lib/testing/mock-logger.js';
 import { format, toZonedTime } from 'date-fns-tz';
@@ -233,6 +233,88 @@ describe('controller.js', () => {
 				),
 				true
 			);
+		});
+	});
+	describe('buildPostCases', () => {
+		beforeEach(() => {
+			mockGetCbosApiClientForSession.mock.resetCalls();
+			mockCbosApiClient.patchAppeal.mock.resetCalls();
+		});
+		const mockCbosApiClient = {
+			patchAppeal: mock.fn()
+		};
+		const mockGetCbosApiClientForSession = mock.fn();
+		mockGetCbosApiClientForSession.mock.mockImplementation(() => mockCbosApiClient);
+		const mockService = () => {
+			return {
+				logger: mockLogger(),
+				getCbosApiClientForSession: mockGetCbosApiClientForSession
+			};
+		};
+
+		test('should update one case', async () => {
+			const service = mockService();
+			const req = { body: { inspectorId: 'inspectorId', selectedCases: '1' } };
+			const res = { redirect: mock.fn() };
+			const controller = buildPostCases(service);
+			await controller(req, res);
+			assert.strictEqual(mockGetCbosApiClientForSession.mock.callCount(), 1);
+			assert.strictEqual(mockCbosApiClient.patchAppeal.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.calls[0].arguments[0], '/?inspectorId=inspectorId');
+		});
+
+		test('should update list of cases', async () => {
+			const service = mockService();
+			const req = { body: { inspectorId: 'inspectorId', selectedCases: ['1', '2', '3'] } };
+			const res = { redirect: mock.fn() };
+			const controller = buildPostCases(service);
+			await controller(req, res);
+			assert.strictEqual(mockGetCbosApiClientForSession.mock.callCount(), 1);
+			assert.strictEqual(mockCbosApiClient.patchAppeal.mock.callCount(), 3);
+			assert.strictEqual(res.redirect.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.calls[0].arguments[0], '/?inspectorId=inspectorId');
+		});
+	});
+	describe('buildPostCases', () => {
+		beforeEach(() => {
+			mockGetCbosApiClientForSession.mock.resetCalls();
+			mockCbosApiClient.patchAppeal.mock.resetCalls();
+		});
+		const mockCbosApiClient = {
+			patchAppeal: mock.fn()
+		};
+		const mockGetCbosApiClientForSession = mock.fn();
+		mockGetCbosApiClientForSession.mock.mockImplementation(() => mockCbosApiClient);
+		const mockService = () => {
+			return {
+				logger: mockLogger(),
+				getCbosApiClientForSession: mockGetCbosApiClientForSession
+			};
+		};
+
+		test('should update one case', async () => {
+			const service = mockService();
+			const req = { body: { inspectorId: 'inspectorId', selectedCases: '1' } };
+			const res = { redirect: mock.fn() };
+			const controller = buildPostCases(service);
+			await controller(req, res);
+			assert.strictEqual(mockGetCbosApiClientForSession.mock.callCount(), 1);
+			assert.strictEqual(mockCbosApiClient.patchAppeal.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.calls[0].arguments[0], '/?inspectorId=inspectorId');
+		});
+
+		test('should update list of cases', async () => {
+			const service = mockService();
+			const req = { body: { inspectorId: 'inspectorId', selectedCases: ['1', '2', '3'] } };
+			const res = { redirect: mock.fn() };
+			const controller = buildPostCases(service);
+			await controller(req, res);
+			assert.strictEqual(mockGetCbosApiClientForSession.mock.callCount(), 1);
+			assert.strictEqual(mockCbosApiClient.patchAppeal.mock.callCount(), 3);
+			assert.strictEqual(res.redirect.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.calls[0].arguments[0], '/?inspectorId=inspectorId');
 		});
 	});
 });
