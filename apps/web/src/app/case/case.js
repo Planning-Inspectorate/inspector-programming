5ff81ffc768cfc98997ad4fe5b02326d8591213d
gmmagruder <gabrielle.magruder@gmail.com>
2025-08-21 15:46:03 +0100

    feat: add error when update cases in cbos fails

--- apps/web/src/app/case/case.js
+++ apps/web/src/app/case/case.js
@@ -14,25 +14,33 @@ export async function getCaseDetails(db, caseId) {
 }
 
 /**
+ * Assigns cases to inspector
+ * Returns case ids that failed to be updated
  * @param {import('../auth/session.service').SessionWithAuth} session
  * @param {import('#service').WebService} service
  * @param {string} inspectorId
  * @param {string[]} caseIds
+ * @returns {Promise<string[]>}
  */
 export async function assignCasesToInspector(session, service, inspectorId, caseIds) {
 	if (inspectorId == null || inspectorId == '') {
 		service.logger.warn('No inspector selected');
-		return;
+		return caseIds;
 	}
 
 	const cbosApiClient = service.getCbosApiClientForSession(session);
 	const appealPatchData = { inspector: inspectorId };
+
+	let failedCases = [];
 
 	for (let caseId of caseIds) {
 		try {
 			await cbosApiClient.patchAppeal(caseId, appealPatchData);
 		} catch (error) {
 			service.logger.error(error, 'Failed to update case');
+			failedCases.push(caseId);
 		}
 	}
+
+	return failedCases;
 }
