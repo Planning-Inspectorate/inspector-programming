d675a33ba116958a674ee3f4d46c470c048b7c3d
james.barnes <james.barnes@solirius.com>
2025-08-26 12:40:32 +0100

    refactor: migrate Inspector types over to package to enable use across both

--- packages/lib/data/types.d.ts
+++ packages/lib/data/types.d.ts
@@ -34,3 +34,48 @@ export interface Filters {
 	minimumAge?: string;
 	maximumAge?: string;
 }
+
+export interface Inspector {
+	id: string;
+	firstName: string;
+	lastName: string;
+	emailAddress: string;
+	address?: Address;
+
+	grade?: string;
+	fte?: number;
+
+	inspectorManager?: boolean;
+	chartingOfficerId?: string;
+
+	specialisms?: Specialism[];
+	preclusions?: Preclusion[];
+}
+
+export interface Address {
+	addressLine1?: string;
+	addressLine2?: string;
+	townCity?: string;
+	county?: string;
+	postcode?: string;
+}
+
+export interface Specialism {
+	name: string;
+	proficiency: 'trained' | 'in-training';
+	validFrom: Date | string;
+}
+
+export type Preclusion = LpaPreclusion | LocationPreclusion | OrganisationPreclusion;
+
+export interface LpaPreclusion {
+	lpaId: string;
+}
+
+export interface LocationPreclusion {
+	postcode: string;
+}
+
+export interface OrganisationPreclusion {
+	organisation: string;
+}
