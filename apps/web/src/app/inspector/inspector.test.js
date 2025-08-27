287f96d4521917c9899e9f47e24953879bf326a0
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-08-27 08:43:51 +0100

    test(web): getInspectorDetails tests

--- apps/web/src/app/inspector/inspector.test.js
+++ apps/web/src/app/inspector/inspector.test.js
@@ -1,6 +1,12 @@
 import { describe, it, mock } from 'node:test';
 import { strict as assert } from 'node:assert';
-import { getInspectorById, fetchInspectorList, getSortedInspectorList, getInspectorList } from './inspector.js';
+import {
+	getInspectorById,
+	fetchInspectorList,
+	getSortedInspectorList,
+	getInspectorList,
+	getInspectorDetails
+} from './inspector.js';
 
 const groupId = 'groupId';
 const mockSession = {};
@@ -440,5 +446,35 @@ describe('inspectors', () => {
 		mockEntraClient.listAllGroupMembers.mock.mockImplementationOnce(() => groupMemberList);
 		const inspectorList = await getInspectorList(mockService, mockSessionWithAccount);
 		assert.deepStrictEqual(inspectorList, expectedInspectorList);
+	});
+});
+
+describe('getInspectorDetails', () => {
+	it('should return null if no inspectorId is provided', async () => {
+		const mockDb = {
+			inspectors: {
+				findUnique: mock.fn()
+			}
+		};
+		const result = await getInspectorDetails(mockDb, undefined);
+		assert.equal(result, null);
+	});
+	it('should return inspector if inspectorId is provided', async () => {
+		const mockInspector = {
+			id: '1',
+			firstName: 'John',
+			lastName: 'Doe'
+		};
+		const mockDb = {
+			inspector: {
+				findFirst: mock.fn(() => mockInspector)
+			}
+		};
+		const result = await getInspectorDetails(mockDb, 'entra-id-1');
+		assert.equal(result, mockInspector);
+		assert.equal(mockDb.inspector.findFirst.mock.callCount(), 1);
+		const args = mockDb.inspector.findFirst.mock.calls[0].arguments[0];
+		assert.deepEqual(args.where?.entraId, 'entra-id-1');
+		assert.deepEqual(args?.include, { Specialisms: true });
 	});
 });
