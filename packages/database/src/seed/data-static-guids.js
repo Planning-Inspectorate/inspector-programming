4583ed421ea6b8a209686444e5a37e157ff6c29d
james.barnes <james.barnes@solirius.com>
2025-09-02 10:25:10 +0100

    chore: seed in static calendar rules data

---
+++ packages/database/src/seed/data-static-guids.js
@@ -0,0 +1,7 @@
+export const calendarEventTimingIds = [
+	'746d4006-82f6-4631-875f-75dbf66c3271',
+	'b8db4d4e-ba13-4378-ba1a-e6fc0629a023',
+	'e2d13461-64ac-4ab4-a4f5-4b7cd9bc5220',
+	'd4f53ef1-ab2d-4e65-8f9c-97eaf4dce20b',
+	'09500a7a-e790-4c42-b89d-c80b849783d6'
+];
