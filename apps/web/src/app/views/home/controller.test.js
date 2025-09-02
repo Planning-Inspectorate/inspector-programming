64dec43cc9830b3ff2b7baae9faca4d270efabaf
gmmagruder <gabrielle.magruder@gmail.com>
2025-09-02 10:16:14 +0100

    test: add unit test to check if inspector coordinates filter is applied

--- apps/web/src/app/views/home/controller.test.js
+++ apps/web/src/app/views/home/controller.test.js
@@ -75,6 +75,9 @@ describe('controller.js', () => {
 			const controller = buildViewHome(service);
 			await controller(req, res);
 			assert.strictEqual(service.casesClient.getCases.mock.callCount(), 1);
+			assert.deepStrictEqual(service.casesClient.getCases.mock.calls[0].arguments[0], {
+				inspectorCoordinates: { lat: 51.4508591, lng: -2.5828931 }
+			});
 			assert.strictEqual(service.db.inspector.findFirst.mock.callCount(), 1);
 			assert.strictEqual(res.render.mock.callCount(), 1);
 			const args = res.render.mock.calls[0].arguments[1];
@@ -111,6 +114,30 @@ describe('controller.js', () => {
 				args.appeals.cases.map((c) => c.id),
 				[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 			);
+		});
+		test('should not filter cases by inspector coordinates if inspector details is missing', async () => {
+			const service = mockService();
+			entraClient.listAllGroupMembers.mock.mockImplementationOnce(() => [
+				{ id: 'inspector-id', name: 'Test Inspector' }
+			]);
+			service.casesClient.getCases.mock.mockImplementationOnce(() => ({
+				cases: Array.from({ length: 10 }, (_, i) => ({ id: i + 1, caseAge: i * 5 })),
+				total: 10
+			}));
+			const req = {
+				url: '/?inspectorId=inspector-id',
+				query: { inspectorId: 'inspector-id' },
+				session: { account: { idTokenClaims: { groups: ['inspectors-group-id'] }, localAccountId: 'inspector-id' } }
+			};
+			const res = { render: mock.fn() };
+			const controller = buildViewHome(service);
+			await controller(req, res);
+			assert.strictEqual(service.casesClient.getCases.mock.callCount(), 1);
+			assert.deepStrictEqual(service.casesClient.getCases.mock.calls[0].arguments[0], {});
+			assert.strictEqual(service.db.inspector.findFirst.mock.callCount(), 1);
+			assert.strictEqual(res.render.mock.callCount(), 1);
+			const args = res.render.mock.calls[0].arguments[1];
+			assert.strictEqual(args.appeals?.cases?.length, 10);
 		});
 	});
 	describe('buildPostHome', () => {
