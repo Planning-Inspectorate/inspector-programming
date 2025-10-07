ec78a2fd7b014067e09c75d342fc910744a08b8d
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-10-07 10:19:12 +0100

    feat(api): limit /event call concurrency

--- apps/web/src/app/api/api-service.js
+++ apps/web/src/app/api/api-service.js
@@ -6,6 +6,11 @@ import { EntraClient } from '@pins/inspector-programming-lib/graph/entra.js';
 import { CachedEntraClient } from '@pins/inspector-programming-lib/graph/cached-entra-client.js';
 
 export class ApiService {
+	#isFetchingEvents = false;
+	/** @type {Promise<import('./types').CalendarEvent[]>|null} */
+	#fetchingEventsPromise = null;
+	#logger;
+
 	/**
 	 * @param {import('../service.js').WebService} service
 	 */
@@ -22,5 +27,40 @@ export class ApiService {
 
 		const entraClient = new EntraClient(client);
 		this.entraClient = new CachedEntraClient(entraClient, apiCache);
+
+		this.#logger = service.logger.child({
+			isApi: true
+		});
+	}
+
+	get logger() {
+		return this.#logger;
+	}
+
+	/**
+	 * Is the system fetching events from user calendars?
+	 * @returns {boolean}
+	 */
+	get isFetchingEvents() {
+		return this.#isFetchingEvents;
+	}
+
+	set isFetchingEvents(isFetchingEvents) {
+		this.#isFetchingEvents = isFetchingEvents;
+		if (!isFetchingEvents) {
+			this.#fetchingEventsPromise = null;
+		}
+	}
+
+	/**
+	 * If the system is fetching events, this is the promise which will resolve with the events
+	 * @returns {Promise<import('./types').CalendarEvent[]>|null}
+	 */
+	get fetchingEventsPromise() {
+		return this.#fetchingEventsPromise;
+	}
+
+	set fetchingEventsPromise(fetchingEventsPromise) {
+		this.#fetchingEventsPromise = fetchingEventsPromise;
 	}
 }
