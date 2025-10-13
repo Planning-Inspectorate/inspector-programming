e87366c631d35c6141f4ea355bf7d7975daaff6b
james.barnes <james.barnes@solirius.com>
2025-10-13 12:48:44 +0100

    chore: update event timing rules with listed building rules

--- packages/database/src/seed/data-static-guids.js
+++ packages/database/src/seed/data-static-guids.js
@@ -3,5 +3,7 @@ export const calendarEventTimingIds = [
 	'b8db4d4e-ba13-4378-ba1a-e6fc0629a023',
 	'e2d13461-64ac-4ab4-a4f5-4b7cd9bc5220',
 	'd4f53ef1-ab2d-4e65-8f9c-97eaf4dce20b',
-	'09500a7a-e790-4c42-b89d-c80b849783d6'
+	'09500a7a-e790-4c42-b89d-c80b849783d6',
+	'e69dd26d-7bdf-4aa8-bba0-9e454f47f487',
+	'd21e9b97-bf60-42d2-981f-56b0990f645c'
 ];
