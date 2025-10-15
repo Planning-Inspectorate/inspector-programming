a49642bb0f149f5c0753bf65e21fa5391aff7691
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-10-15 10:11:40 +0100

    refactor(database): make calendar timing rules easier to read

---
+++ packages/database/src/seed/data-static-calendar-event-timings.ts
@@ -0,0 +1,161 @@
+// @ts-ignore - these do exist and work!
+import { APPEAL_ALLOCATION_LEVEL, APPEAL_CASE_PROCEDURE, APPEAL_CASE_TYPE } from '@planning-inspectorate/data-model';
+import { calendarEventTimingIds } from './data-static-guids.js';
+import type { Prisma } from '@pins/inspector-programming-database/src/client/client.ts';
+
+interface CalendarEventTimingRule extends Omit<Prisma.CalendarEventTimingCreateInput, 'CalendarEventTimingRules'> {
+	AppliesTo: {
+		caseType: string;
+		caseProcedure: string;
+		allocationLevels: string[];
+	}[];
+}
+
+const allocationLevels = {
+	F_G_H: [APPEAL_ALLOCATION_LEVEL.F, APPEAL_ALLOCATION_LEVEL.G, APPEAL_ALLOCATION_LEVEL.H],
+	A_TO_E: [
+		APPEAL_ALLOCATION_LEVEL.A,
+		APPEAL_ALLOCATION_LEVEL.B,
+		APPEAL_ALLOCATION_LEVEL.C,
+		APPEAL_ALLOCATION_LEVEL.D,
+		APPEAL_ALLOCATION_LEVEL.E
+	]
+};
+
+const calendarEventTimingsRules: CalendarEventTimingRule[] = [
+	{
+		id: calendarEventTimingIds[0],
+		AppliesTo: [
+			{
+				caseType: APPEAL_CASE_TYPE.D, // householder
+				caseProcedure: APPEAL_CASE_PROCEDURE.WRITTEN,
+				allocationLevels: allocationLevels.F_G_H
+			}
+		],
+		prepTime: 0,
+		siteVisitTime: 2,
+		reportTime: 6,
+		costsTime: 2
+	},
+	{
+		id: calendarEventTimingIds[1],
+		AppliesTo: [
+			{
+				caseType: APPEAL_CASE_TYPE.D, // householder
+				caseProcedure: APPEAL_CASE_PROCEDURE.WRITTEN,
+				allocationLevels: allocationLevels.A_TO_E
+			}
+		],
+		prepTime: 0,
+		siteVisitTime: 2,
+		reportTime: 10,
+		costsTime: 2
+	},
+	{
+		id: calendarEventTimingIds[2],
+		AppliesTo: [
+			{
+				caseType: APPEAL_CASE_TYPE.ZP, // CAS Planning
+				caseProcedure: APPEAL_CASE_PROCEDURE.WRITTEN,
+				allocationLevels: [APPEAL_ALLOCATION_LEVEL.H]
+			},
+			{
+				caseType: APPEAL_CASE_TYPE.ZA, // CAS Advert
+				caseProcedure: APPEAL_CASE_PROCEDURE.WRITTEN,
+				allocationLevels: [APPEAL_ALLOCATION_LEVEL.H]
+			}
+		],
+		prepTime: 2,
+		siteVisitTime: 2,
+		reportTime: 4,
+		costsTime: 2
+	},
+	{
+		id: calendarEventTimingIds[3],
+		AppliesTo: [
+			{
+				caseType: APPEAL_CASE_TYPE.W, // S78/Planning
+				caseProcedure: APPEAL_CASE_PROCEDURE.WRITTEN,
+				allocationLevels: allocationLevels.F_G_H
+			}
+		],
+		prepTime: 2,
+		siteVisitTime: 2,
+		reportTime: 8,
+		costsTime: 4
+	},
+	{
+		id: calendarEventTimingIds[4],
+		AppliesTo: [
+			{
+				caseType: APPEAL_CASE_TYPE.W, // S78/Planning
+				caseProcedure: APPEAL_CASE_PROCEDURE.WRITTEN,
+				allocationLevels: allocationLevels.A_TO_E
+			}
+		],
+		prepTime: 2,
+		siteVisitTime: 2,
+		reportTime: 12,
+		costsTime: 4
+	},
+	{
+		id: calendarEventTimingIds[5],
+		AppliesTo: [
+			{
+				caseType: APPEAL_CASE_TYPE.Y, // Listed building
+				caseProcedure: APPEAL_CASE_PROCEDURE.WRITTEN,
+				allocationLevels: allocationLevels.F_G_H
+			}
+		],
+		prepTime: 2,
+		siteVisitTime: 2,
+		reportTime: 8,
+		costsTime: 4
+	},
+	{
+		id: calendarEventTimingIds[6],
+		AppliesTo: [
+			{
+				caseType: APPEAL_CASE_TYPE.Y, // Listed building
+				caseProcedure: APPEAL_CASE_PROCEDURE.WRITTEN,
+				allocationLevels: allocationLevels.A_TO_E
+			}
+		],
+		prepTime: 2,
+		siteVisitTime: 2,
+		reportTime: 12,
+		costsTime: 4
+	}
+];
+
+function ruleToCreateInput(rule: CalendarEventTimingRule): Prisma.CalendarEventTimingCreateInput {
+	const rules: Prisma.CalendarEventTimingRuleCreateOrConnectWithoutCalendarEventTimingInput[] = [];
+	for (const appliesTo of rule.AppliesTo) {
+		for (const allocationLevel of appliesTo.allocationLevels) {
+			const caseType_caseProcedure_allocationLevel = {
+				caseType: appliesTo.caseType,
+				caseProcedure: appliesTo.caseProcedure,
+				allocationLevel
+			};
+			rules.push({
+				where: {
+					caseType_caseProcedure_allocationLevel
+				},
+				create: caseType_caseProcedure_allocationLevel
+			});
+		}
+	}
+	return {
+		id: rule.id,
+		CalendarEventTimingRules: {
+			connectOrCreate: rules
+		},
+		prepTime: rule.prepTime,
+		siteVisitTime: rule.siteVisitTime,
+		reportTime: rule.reportTime,
+		costsTime: rule.costsTime
+	};
+}
+
+export const calendarEventTimings: Prisma.CalendarEventTimingCreateInput[] =
+	calendarEventTimingsRules.map(ruleToCreateInput);
