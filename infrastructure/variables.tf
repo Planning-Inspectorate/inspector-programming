1920626f56ede92497df887347794ee55cc6196a
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2025-11-18 15:51:45 +0000

    chore(infrastructure): adding daily log on analytics workspace

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -99,6 +99,14 @@ variable "health_check_eviction_time_in_min" {
   default     = 10
 }
 
+variable "monitoring_config" {
+  description = "Config for monitoring"
+  type = object({
+    web_app_insights_web_test_enabled = bool
+    log_daily_cap                     = number
+  })
+}
+
 
 variable "sql_config" {
   description = "Config for SQL Server and DB"
