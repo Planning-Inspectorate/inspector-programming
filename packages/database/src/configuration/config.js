b8688d875cdb167d17347321029924ed5bf788ec
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-02-19 14:32:17 +0000

    chore(database): add prod seed script and ensure .env is loaded

--- packages/database/src/configuration/config.js
+++ packages/database/src/configuration/config.js
@@ -1,9 +1,14 @@
+import { loadEnvFile } from 'node:process';
+
 /**
  * Load configuration for seeding the database
  *
  * @returns {{db?: string}}
  */
 export function loadConfig() {
+	// load configuration from .env file into process.env
+	// prettier-ignore
+	try {loadEnvFile()} catch {/* ignore errors*/}
 	return {
 		db: process.env.SQL_CONNECTION_STRING
 	};
