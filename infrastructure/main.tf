2135f3fccc20a60a242aea0b8ccdbc70bff77320
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-03-02 14:59:49 +0000

    feat: turning on private endpoints for key vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,29 +13,28 @@ resource "azurerm_resource_group" "secondary" {
 }
 
 resource "azurerm_key_vault" "main" {
+  name                          = "${local.org}-kv-${local.shorter_resource_suffix}"
+  location                      = module.primary_region.location
+  resource_group_name           = azurerm_resource_group.primary.name
+  enabled_for_disk_encryption   = true
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days    = 7
+  purge_protection_enabled      = true
+  rbac_authorization_enabled    = true
+  public_network_access_enabled = false
+  sku_name                      = "standard"
 
-  # checkov:skip=CKV_AZURE_189: Consider moving to VPN; requires RBAC
-  # checkov:skip=CKV_AZURE_109: Route traffic via a VNet; Private Endpoint consideration
-  # checkov:skip=CKV2_AZURE_32: Private Endpoint relies on the VNet
-
-  name                        = "${local.org}-kv-${local.shorter_resource_suffix}"
-  location                    = module.primary_region.location
-  resource_group_name         = azurerm_resource_group.primary.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
-  enable_rbac_authorization   = true
-  sku_name                    = "standard"
+  network_acls {
+    bypass         = "AzureServices"
+    default_action = "Deny"
+  }
 
   tags = local.tags
 }
 
 # secrets to be manually populated
 resource "azurerm_key_vault_secret" "manual_secrets" {
-
   #checkov:skip=CKV_AZURE_41: expiration not valid
-
   for_each = toset(local.secrets)
 
   key_vault_id = azurerm_key_vault.main.id
@@ -43,11 +42,32 @@ resource "azurerm_key_vault_secret" "manual_secrets" {
   value        = "<terraform_placeholder>"
   content_type = "plaintext"
 
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.keyvault]
+
   tags = local.tags
 
   lifecycle {
     ignore_changes = [
       value
     ]
+  }
+}
+
+resource "azurerm_private_endpoint" "keyvault" {
+  name                = "${local.org}-pe-keyvault-${local.resource_suffix}"
+  location            = module.primary_region.location
+  resource_group_name = azurerm_resource_group.primary.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_dns_zone_group {
+    name                 = "${local.org}-pdns-${local.service_name}-keyvault-${var.environment}"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.keyvault.id]
+  }
+
+  private_service_connection {
+    name                           = "${local.org}-psc-keyvault-${local.resource_suffix}"
+    private_connection_resource_id = azurerm_key_vault.main.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
   }
 }
