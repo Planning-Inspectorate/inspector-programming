90ff9f57b3f43c8a47cf7533d6747d441acc3e13
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-03-03 13:00:16 +0000

    style: selected filter header sizes

--- apps/web/src/util/config-middleware.js
+++ apps/web/src/util/config-middleware.js
@@ -6,7 +6,7 @@ export function addLocalsConfiguration() {
 	return (req, res, next) => {
 		res.locals.config = {
 			headerTitle: 'Programme appeals',
-			styleFile: 'style-ef8110e5.css'
+			styleFile: 'style-1302a8fd.css'
 		};
 		next();
 	};
