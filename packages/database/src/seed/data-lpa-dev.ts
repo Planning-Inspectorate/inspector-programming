6d3376e6ec983ebf1001c0f73836ea85865bcb62
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-03-17 12:12:41 +0000

    chore(database): update all LPAs from manage appeals, with names

--- packages/database/src/seed/data-lpa-dev.ts
+++ packages/database/src/seed/data-lpa-dev.ts
@@ -1,15 +1,17 @@
 import { LPA_REGION_IDS } from './lpa-regions.js';
 
+// generated content (using `fetch-lpas.ts), so no need to format only multiple lines
 // Test LPAs from Manage appeals
+// prettier-ignore
 export const LPAS_DEV = Object.freeze([
-	{ lpaCode: 'Q9999', LpaRegion: { connect: { id: LPA_REGION_IDS.NORTH_1 } } },
-	{ lpaCode: 'BASI', LpaRegion: { connect: { id: LPA_REGION_IDS.EAST_1 } } },
-	{ lpaCode: 'WAVE', LpaRegion: { connect: { id: LPA_REGION_IDS.WEST_1 } } },
-	{ lpaCode: 'BRIS', LpaRegion: { connect: { id: LPA_REGION_IDS.NORTH_2 } } },
-	{ lpaCode: 'DORS', LpaRegion: { connect: { id: LPA_REGION_IDS.EAST_2 } } },
-	{ lpaCode: 'WILT', LpaRegion: { connect: { id: LPA_REGION_IDS.WEST_2 } } },
-	{ lpaCode: 'MAID', LpaRegion: { connect: { id: LPA_REGION_IDS.NORTH_3 } } },
-	{ lpaCode: 'BARN', LpaRegion: { connect: { id: LPA_REGION_IDS.EAST_3 } } },
-	{ lpaCode: 'WORT', LpaRegion: { connect: { id: LPA_REGION_IDS.WEST_3 } } },
-	{ lpaCode: 'Q1111', LpaRegion: { connect: { id: LPA_REGION_IDS.NORTH_4 } } }
+    {lpaCode: "Q9999", lpaName: "System Test Borough Council", LpaRegion: { connect: { id: LPA_REGION_IDS.NORTH_1 } }},
+    {lpaCode: "Q1111", lpaName: "System Test Borough Council 2", LpaRegion: { connect: { id: LPA_REGION_IDS.NORTH_4 } }},
+    {lpaCode: "MAID", lpaName: "Maidstone Borough Council", LpaRegion: { connect: { id: LPA_REGION_IDS.NORTH_3 } }},
+    {lpaCode: "BARN", lpaName: "Barnsley Metropolitan Borough Council", LpaRegion: { connect: { id: LPA_REGION_IDS.EAST_3 } }},
+    {lpaCode: "WORT", lpaName: "Worthing Borough Council", LpaRegion: { connect: { id: LPA_REGION_IDS.WEST_3 } }},
+    {lpaCode: "DORS", lpaName: "Dorset Council", LpaRegion: { connect: { id: LPA_REGION_IDS.EAST_2 } }},
+    {lpaCode: "BASI", lpaName: "Basingstoke and Deane Borough Council", LpaRegion: { connect: { id: LPA_REGION_IDS.EAST_1 } }},
+    {lpaCode: "WILT", lpaName: "Wiltshire Council", LpaRegion: { connect: { id: LPA_REGION_IDS.WEST_2 } }},
+    {lpaCode: "WAVE", lpaName: "Waveney District Council", LpaRegion: { connect: { id: LPA_REGION_IDS.WEST_1 } }},
+    {lpaCode: "BRIS", lpaName: "Bristol City Council", LpaRegion: { connect: { id: LPA_REGION_IDS.NORTH_2 } }},
 ]);
