514503ab3bcea4384854c6ab1f33057bb3d7cc45
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-04-22 13:01:54 +0100

    fix(database): only seed cases if configured

--- packages/database/src/configuration/config.js
+++ packages/database/src/configuration/config.js
@@ -3,13 +3,14 @@ import { loadEnvFile } from 'node:process';
 /**
  * Load configuration for seeding the database
  *
- * @returns {{db?: string}}
+ * @returns {{db?: string, seedMockCases: boolean}}
  */
 export function loadConfig() {
 	// load configuration from .env file into process.env
 	// prettier-ignore
 	try {loadEnvFile()} catch {/* ignore errors*/}
 	return {
-		db: process.env.SQL_CONNECTION_STRING
+		db: process.env.SQL_CONNECTION_STRING,
+		seedMockCases: process.env.SEED_MOCK_CASES === 'true'
 	};
 }
