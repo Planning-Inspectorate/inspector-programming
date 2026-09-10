729b00132a399686b324dc67f98ebafbc9495a88
abel-olagoke <Abel.Olagoke.fa@planninginspectorate.gov.uk>
2026-09-10 11:32:03 +0100

    feat(filtering): add support for including special circumstances in case filtering

--- packages/lib/data/special-circumstances.js
+++ packages/lib/data/special-circumstances.js
@@ -2,5 +2,9 @@ export const SPECIAL_CIRCUMSTANCES = Object.freeze({
 	EXCLUDE_GREEN_BELT: 'exclude-green-belt',
 	EXCLUDE_PRIOR_APPROVAL: 'exclude-prior-approval',
 	EXCLUDE_CONDITIONS: 'exclude-conditions',
-	EXCLUDE_DESIGNATED_SITES: 'exclude-designated-sites'
+	EXCLUDE_DESIGNATED_SITES: 'exclude-designated-sites',
+	INCLUDE_GREEN_BELT: 'include-green-belt',
+	INCLUDE_PRIOR_APPROVAL: 'include-prior-approval',
+	INCLUDE_CONDITIONS: 'include-conditions',
+	INCLUDE_DESIGNATED_SITES: 'include-designated-sites'
 });
